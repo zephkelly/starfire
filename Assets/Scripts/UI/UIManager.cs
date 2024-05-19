80898be8490707101b0e3d4567167d8c8a732917
zephkelly <masterevank@gmail.com>
2024-05-19 23:14:14 +1000

    Convert minor alert system to queue

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -8,6 +8,9 @@ public class UIManager : MonoBehaviour
     [Header("UI Messages")]
     public static UIManager Instance;
     public TextMeshProUGUI minorAlert;
+    private bool isMinorAlertActive = false;
+
+    private Queue minorAlertQueue = new Queue();
 
     void Awake()
     {
@@ -21,17 +24,30 @@ public class UIManager : MonoBehaviour
         }
     }
 
-    public void DisplayMinorAlert(string alertText)
+    private void Update()
     {
-        minorAlert.text = alertText;
-        StartCoroutine(FadeTextEffect(minorAlert));
+        if (minorAlertQueue.Count > 0 && !isMinorAlertActive)
+        {
+            StartCoroutine(FadeTextEffect(minorAlert, minorAlertQueue.Dequeue().ToString()));
+        }
     }
 
-    public IEnumerator FadeTextEffect(TextMeshProUGUI text)
+    public void DisplayMinorAlert(string alertText)
     {
+        minorAlertQueue.Enqueue(alertText);
+    }
+
+    public IEnumerator FadeTextEffect(TextMeshProUGUI text, string message)
+    {
+        text.text = message;
+        isMinorAlertActive = true;
+
         text.CrossFadeAlpha(0, 0, false);
-        text.CrossFadeAlpha(1, 2.5f, false);
+        text.CrossFadeAlpha(1, 2f, false);
         yield return new WaitForSeconds(3);
         text.CrossFadeAlpha(0, 2f, false);
+        yield return new WaitForSeconds(2);
+
+        isMinorAlertActive = false;
     }
 }
