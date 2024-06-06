26030202d5aa70954c6890540ab7129f780c83ac
ekelly <masterevank@gmail.com>
2024-06-06 15:32:26 +1000

    Refactor ShipConfiguration

--- Assets/Scripts/Ship/ShipConfiguration.cs
+++ Assets/Scripts/Ship/ShipConfiguration.cs
@@ -3,81 +3,66 @@ using Starfire.Items;
 
 namespace Starfire
 {
-  public interface IShipConfiguration
-  {
-    int EngineTopSpeed { get; }
-    int MaxHealth { get; }
-    int Health { get; }
-    int MaxFuel { get; }
-    int Fuel { get; }
-    int MaxCargo { get; }
-    int Cargo { get; }
-    int Damage(int damage, DamageType damageType);
-    void Repair(int repair, DamageType damageType);
-    void Refuel(int fuel);
-    void LoadCargo(ItemType itemType, int amount);
-  }
-
-  public enum DamageType
-  {
-    Hull,
-    Shield
-  }
-
-  public class ShipConfiguration : ScriptableObject, IShipConfiguration
-  {
-    public int Health { get; private set; }
-    public int Fuel { get; private set; }
-    public int Cargo { get; private set; }
-    public int EngineTopSpeed { get; private set; }
-    public int MaxHealth { get; private set; }
-    public int MaxFuel { get; private set; }
-    public int MaxCargo { get; private set; }
-
-    public int Damage(int damage, DamageType damageType)
+    public enum DamageType
     {
-      if (damageType == DamageType.Hull) return DamageHull(damage);
-      else if (damageType == DamageType.Shield) return DamageShield(damage);
-
-      return -1;
+        Hull,
+        Shield
     }
 
-    public void Repair(int repair, DamageType damageType)
+    public class ShipConfiguration : ScriptableObject
     {
-      if (damageType == DamageType.Hull) RepairHull(repair);
-      else if (damageType == DamageType.Shield) RepairShield(repair);
-    }
+        public int Health { get; private set; }
+        public int Fuel { get; private set; }
+        public int Cargo { get; private set; }
+        public int EngineTopSpeed { get; private set; }
+        public int MaxHealth { get; private set; }
+        public int MaxFuel { get; private set; }
+        public int MaxCargo { get; private set; }
 
-    public void Refuel(int fuel)
-    {
-      //To be implemented
-    }
+        public int Damage(int damage, DamageType damageType)
+        {
+            if (damageType == DamageType.Hull) return DamageHull(damage);
+            else if (damageType == DamageType.Shield) return DamageShield(damage);
 
-    public void LoadCargo(ItemType itemType, int amount)
-    {
-      //To be implemented
-    }
+            return -1;
+        }
 
-    private int DamageHull(int damage)
-    {
-      //To be implemented
-      return -1;
-    }
+        public void Repair(int repair, DamageType damageType)
+        {
+            if (damageType == DamageType.Hull) RepairHull(repair);
+            else if (damageType == DamageType.Shield) RepairShield(repair);
+        }
 
-    private int DamageShield(int damage)
-    {
-      //To be implemented
-      return -1;
-    }
+        public void Refuel(int fuel)
+        {
+            //To be implemented
+        }
 
-    private void RepairHull(int repair)
-    {
-      //To be implemented
-    }
+        public void LoadCargo(ItemType itemType, int amount)
+        {
+            //To be implemented
+        }
 
-    private void RepairShield(int repair)
-    {
-      //To be implemented
+        private int DamageHull(int damage)
+        {
+            //To be implemented
+            return -1;
+        }
+
+        private int DamageShield(int damage)
+        {
+            //To be implemented
+            return -1;
+        }
+
+        private void RepairHull(int repair)
+        {
+            //To be implemented
+        }
+
+        private void RepairShield(int repair)
+        {
+            //To be implemented
+        }
     }
-  }
 }
